1b8b6484e10e60db386ed88214bf03d9b48086c7
Tee Johnson <johnsontee777@gmail.com>
2021-03-13 18:20:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+# x=5
+# y=6
+
+# if x<y:
+#    print('Yes')
+
+#==========
+# if 'Hello' in 'Hello World!':
+#    print('Yes')
+
+#==================
+
+# x=7
+# y=6
+
+# if x > y:
+#   print('yes it is greater')
+# elif x < y:
+#   print('no it is less')
+# else:
+#   print('Neither')
+  #==================
+
+  # '''
+  # Nested if statement
+  # '''
+# num = -3
+# if num >= 0:
+#     if num==0:
+#       print('Zero')
+
+#     else:
+#       print('Positive number')
+
+# else:
+#     print('Negative number')
+
+# ===================
+# '''
+# one-Line 'if' statements
+# '''
+# if 7>5: print('yes it is')
+
+#==========================
+'''
+Ternary operator
+'''
+# print('Yes') if 4>5 else print('No')
+
+ #or the above can be written in this form below too
+
+if 4>5:
+    print('Yes')
+else:
+    print('No')   
+
+
+  
+
+
+
+
+
+
+
+# num=5
+# if num >=0:
+#   print('Zero')
+# else: 
+#   print('Negative')
